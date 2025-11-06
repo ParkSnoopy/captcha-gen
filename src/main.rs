24b0c0c4868071ac4b929b0103ad7f5404c81b81
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-11-07 02:30:34 +0800

    v0.1.1
      - add `Dropout` constant to drop some captchas

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use lazy_static::lazy_static;
+
 use std::{
     env,
+    cmp::min,
     path::{ PathBuf, Path },
-    sync::{ LazyLock, OnceLock, RwLock, Arc },
+    sync::{ RwLock, Arc },
+};
+use rand::{
+    rng,
+    Rng,
 };
 
 use captcha_rs::{
@@ -21,38 +28,45 @@ const OUTPUT_H: u32 = 100;
 const OUTPUT_INDEX_FROM: usize = 0;
 
 const REPEAT: usize = 1;
-const LENGTH: usize = 4;
+const LENGTH: usize = 5;
+const DROPOUT: f64 = 0.50;
+
+
 
 const SOURCE: &str  = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
 
+lazy_static! {
+    static ref TOTAL: RwLock<usize> = RwLock::new(
+        SOURCE.len().pow(LENGTH as u32) as usize
+    );
+
+    static ref CURRENT: Arc<RwLock<usize>> = Arc::new(RwLock::new(
+        0
+    ));
+
+    static ref OUTPUT_ROOT: PathBuf = {
+        let mut cwd = env::current_dir().expect("Unable to get `CWD`");
+        cwd.push(OUTPUT_POSTFIX);
+        cwd
+    };
+
+    static ref PRINT_EVERY: Arc<RwLock<usize>> = Arc::new(RwLock::new(
+        min(
+            10000,
+            *TOTAL.read().unwrap() / 100,
+        )
+    ));
+}
 
 
-static TOTAL: LazyLock<usize> = LazyLock::new(|| {
-    SOURCE.len().pow(LENGTH as u32) as usize
-});
-const PRINT_EVERY: usize = 10000;
-static CURRENT: LazyLock<Arc<RwLock<usize>>> = LazyLock::new(|| {
-    Arc::new(RwLock::new(0))
-});
-static OUTPUT_ROOT: OnceLock<PathBuf> = OnceLock::new();
 
 fn main() {
-
     // RUNTIME TEST
 
     assert_eq!(SOURCE.len(), 10+26);
 
     //
-
-    OUTPUT_ROOT.set({
-        let mut cwd = env::current_dir().expect("Unable to get `CWD`");
-        cwd.push(OUTPUT_POSTFIX);
-        cwd
-    }).expect("Failed to set `OUTPUT_ROOT`");
-
     let targets: Vec<String> = build( String::new(), true ).right().expect("Unexpected result type from fn `build`");
-
-    assert!(targets.len() == *TOTAL, "Length of `targets` is unexpected. Expected `{}`, Actual `{}`", *TOTAL, targets.len());
 
     // init `CURRENT` for use while `save`ing
     *CURRENT.write().unwrap() = 0;
@@ -71,19 +85,19 @@ fn main() {
                 let capt = string_to_captcha(target.clone());
 
                 capt.save(&path);
-
-                //dbg!(target);
-                //dbg!(capt);
-                //panic!("intended crash");
             }
 
             *CURRENT.write().unwrap() += 1;
-            if *CURRENT.read().unwrap() % PRINT_EVERY == 0 {
-                let prg = (100_f64) * (*CURRENT.read().unwrap() as f64) / (*TOTAL as f64);
-                println!("  - Progress: `{prg:.02?}`% ({}/{})", *CURRENT.read().unwrap(), *TOTAL);
+            if *CURRENT.read().unwrap() % *PRINT_EVERY.read().unwrap() == 0 {
+                let prg = (100_f64) * (*CURRENT.read().unwrap() as f64) / (*TOTAL.read().unwrap() as f64);
+                println!("  - Progress: `{prg:.02?}`% ({}/{})", *CURRENT.read().unwrap(), *TOTAL.read().unwrap());
             }
         })
         .collect();
+
+    // print `100.00 %` after finish
+    let prg = (100_f64) * (*CURRENT.read().unwrap() as f64) / (*TOTAL.read().unwrap() as f64);
+    println!("  - Progress: `{prg:.02?}`% ({}/{})", *CURRENT.read().unwrap(), *TOTAL.read().unwrap());
 }
 
 fn string_to_captcha(s: String) -> Captcha {
@@ -98,12 +112,12 @@ fn string_to_captcha(s: String) -> Captcha {
 }
 
 fn make_path<S: AsRef<Path> + std::fmt::Display>(captcha: S, iteration: usize) -> PathBuf {
-    OUTPUT_ROOT.get().expect("`OUTPUT_ROOT` is not initialized yet").as_path().join(
+    OUTPUT_ROOT.as_path().join(
         format!("{captcha}.{iteration}.{OUTPUT_FORMAT}")
     )
 }
 
-fn build(prefix: String, _is_root: bool) -> Either<String, Vec<String>> {
+fn build(prefix: String, is_root: bool) -> Either<String, Vec<String>> {
     if prefix.len() >= LENGTH {
         *CURRENT.write().unwrap() += 1;
         /*
@@ -123,13 +137,27 @@ fn build(prefix: String, _is_root: bool) -> Either<String, Vec<String>> {
             |v| results.write().unwrap().extend(v),
         );
     }
-    /*
+    let mut results: Vec<String> = results.into_inner().expect("Failed to strip `RwLock` from `results`");
+
     if is_root {
-        // print progress 100% if this is root function of recursion
-        println!("  - Progress: `100.00`% ({}/{})", *CURRENT.read().unwrap(), *TOTAL);
+        let mut rng = rng();
+        results = results
+            .clone()
+            .into_iter()
+            .filter(|&_| { rng.random::<f64>() > DROPOUT })
+            .collect();
+
+        // Overwrite `TOTAL`
+        println!("  - Target is built: ( Expected `{}`, After Dropout `{}` )", TOTAL.read().unwrap(), results.len());
+        *TOTAL.write().unwrap() = results.len();
+
+        // Recalculate `PRINT_EVERY`
+        let print_every = *PRINT_EVERY.read().unwrap(); // prevent Mutex locked forever
+        *PRINT_EVERY.write().unwrap() = min(
+            print_every,
+            *TOTAL.read().unwrap() / 100,
+        );
     }
-    */
-    either::Right(
-        results.into_inner().expect("Failed to strip `RwLock` from `results`")
-    )
+
+    either::Right(results)
 }
